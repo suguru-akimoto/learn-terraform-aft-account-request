7c15799816d9646ef9b92df0e9077d02642c384e
suguru akimoto <suguru.akimoto@accenture.com>
2024-04-03 21:28:37 +0900

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "suguru.akimoto@accenture.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "suguru.akimoto+sandbox@accenture.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
